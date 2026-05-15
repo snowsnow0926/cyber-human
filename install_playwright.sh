231dcd7fe53e58238de98fb98617d08642beb1d0
Ubuntu <ubuntu@localhost.localdomain>
2026-05-16 02:04:45 +0800

    cleanup test files

--- install_playwright.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Install Playwright Chromium - runs on VPS
-cd /home/ubuntu/cyber-human
-source venv/bin/activate
-python3 -m playwright install --with-deps chromium 2>&1 | tee /tmp/playwright_done.log
-echo "DONE" >> /tmp/playwright_done.log
